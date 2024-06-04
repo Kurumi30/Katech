c4e79a47406280f083e7b8b4dc7d8046664b4101
Kurumi30 <fernandoshengxinzhu@gmail.com>
2024-06-04 13:48:47 -0300

    feat: Update meta image and footer links

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('#menu-icon')
 const navbar = document.querySelector('.navbar')
+const imageMetaTag = document.querySelector('meta[property="og:image"]')
 
 menu.onclick = () => {
   menu.classList.toggle('bx-x')
@@ -10,3 +11,11 @@ window.onscroll = () => {
   menu.classList.remove('bx-x')
   navbar.classList.remove('active')
 }
+
+document.querySelectorAll('.footer i').forEach(icon => {
+  icon.addEventListener('click', function () {
+    open(this.children[0].href, '_blank')
+  })
+})
+
+imageMetaTag.setAttribute('content', `${location.href}/img/katech.png`)
